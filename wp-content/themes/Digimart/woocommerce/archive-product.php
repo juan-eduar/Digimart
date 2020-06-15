a8300b3b41e8c266671a33cea905ab9759d6ca54
Gladibeth <gladibethdelosa@gmail.com>
2020-06-15 15:00:22 -0400

    btn whatsap in achiveproduct

--- wp-content/themes/Digimart/woocommerce/archive-product.php
+++ wp-content/themes/Digimart/woocommerce/archive-product.php
@@ -2,18 +2,45 @@
         <div class="selec-product">
 <?php while ( have_posts() ) : ?>
 			<?php the_post(); ?>
-          <a href="<?php the_permalink(); ?>" class=" caption-products samsung" >
+          <div  class=" caption-products samsung" >
             <div class="card hvr-shadow-p">
               <div class="card-img">
-                <div class="img-products" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');"></div>
+                <a href="<?php the_permalink(); ?>">
+                  
+                    <div class="img-products" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');"></div>
+                </a>
               </div>
               <div class="card-content">
                 <div class="caption-title">
-                  <h5 class="open-sans title-product"><?php the_title(); ?></h5>
+                  <a href="<?php the_permalink(); ?>">
+                    <h5 class="open-sans title-product"><?php the_title(); ?></h5>
+                  </a>
+                  <div class="">
+                    <a class=" btn-drop btn-custom btn--medium btn--filled">
+                    Comprar por Whatsapp
+                    </a>
+                  
+                    <div class="dropdown__btn dropdown-item__desktop" aria-labelledby="dropdownMenuLink">
+                      <a class="" href=" <?php echo 'https://web.whatsapp.com/send?phone=5804121271277&text=Hola,%20Estoy%20interesado%20en%20este%20producto%20'.str_replace( ' ', '%20', get_the_title()) . ' ' . urlencode(get_permalink());?>">Asesor de ventas</a> <br>
+                      <a class="" href=" <?php echo 'https://web.whatsapp.com/send?phone=5804121727855&text=Hola,%20Estoy%20interesado%20en%20este%20producto%20'.str_replace( ' ', '%20', get_the_title()) . ' ' . urlencode(get_permalink());?>">Asesor de ventas</a> <br>
+                      <a class="" href="<?php echo 'https://web.whatsapp.com/send?phone=5804126403077&text=Hola,%20Estoy%20interesado%20en%20este%20producto%20'.str_replace( ' ', '%20', get_the_title()) . ' ' . urlencode(get_permalink());?>">Asesor de ventas</a> <br>
+
+                      
+                    </div>
+                    <div class="dropdown__btn dropdown-item__mobile" aria-labelledby="dropdownMenuLink">
+                    
+                    <a class="" href=" <?php echo 'https://api.whatsapp.com/send?phone=5804121271277&text=Hola,%20Estoy%20interesado%20en%20este%20producto%20'.str_replace( ' ', '%20', get_the_title()) . ' ' . urlencode(get_permalink());?>">Asesor de ventas</a> <br>
+                      <a class="" href=" <?php echo 'https://api.whatsapp.com/send?phone=5804121727855&text=Hola,%20Estoy%20interesado%20en%20este%20producto%20'.str_replace( ' ', '%20', get_the_title()) . ' ' . urlencode(get_permalink());?>">Asesor de ventas</a> <br>
+                      <a class="" href="<?php echo 'https://api.whatsapp.com/send?phone=5804126403077&text=Hola,%20Estoy%20interesado%20en%20este%20producto%20'.str_replace( ' ', '%20', get_the_title()) . ' ' . urlencode(get_permalink());?>">Asesor de ventas</a> <br>
+                      
+                    </div>
+
+
+                  </div>
                 </div>
               </div>
             </div>
-          </a>
+    </div>
       <?php endwhile; ?>
 
 </div>
